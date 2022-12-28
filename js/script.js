a3338d4824f90894700c3263d34793668d922cbf
Rasmus Eliasson <ras.muse@hotmail.com>
2022-12-28 21:18:22 +0100

    connected main-nav to hamburger icon

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 function hamburger() {
   let hamburgerIcon = document.querySelector("#hamburger-icon");
+  let mainNavShow = document.querySelector(".main-nav");
   if (hamburgerIcon.getAttribute("src") == "static/hamburger-icon.svg") {
     hamburgerIcon.setAttribute("src", "static/close-icon.svg");
+    mainNavShow.classList.add("show");
   } else {
     hamburgerIcon.setAttribute("src", "static/hamburger-icon.svg");
+    mainNavShow.classList.remove("show");
   }
 }
