b4cd17fa36b3e74ea2ec49d56fb8560adbe59ef1
Rasmus Eliasson <ras.muse@hotmail.com>
2022-12-28 21:05:52 +0100

    toggle hamburger button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+function hamburger() {
+  let hamburgerIcon = document.querySelector("#hamburger-icon");
+  if (hamburgerIcon.getAttribute("src") == "static/hamburger-icon.svg") {
+    hamburgerIcon.setAttribute("src", "static/close-icon.svg");
+  } else {
+    hamburgerIcon.setAttribute("src", "static/hamburger-icon.svg");
+  }
+}
