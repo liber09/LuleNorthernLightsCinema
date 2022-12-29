ce6e5aa3401b35d2889110d1b970deccd948cb53
Rasmus Eliasson <ras.muse@hotmail.com>
2022-12-29 20:45:20 +0100

    beginnign of creating cards

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+/* let data = fetch("src/movies.json")
+  .then((response) => response.json())
+  .then((jsonMovies) => console.log(jsonMovies)); */
+let movies;
+let val = document.querySelector("hello");
 
+val.innerHTML = ' <h1 class="majs"></h1>';
+
+function showCards() {
+  async function display(movies) {
+    let res = await fetch("src/movies.json");
+    let data = await res.json();
+
+    movies = data.movies[0];
+    console.log(movies);
+    return movies;
+  }
+}
+
+showCards(title);
