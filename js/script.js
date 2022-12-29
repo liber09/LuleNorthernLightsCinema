434cfb5c0fc4ae1007a71d6befd00a0384d618ba
Rasmus Eliasson <ras.muse@hotmail.com>
2022-12-29 23:24:46 +0100

    adding cards to frontpage

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-/* let data = fetch("src/movies.json")
-  .then((response) => response.json())
-  .then((jsonMovies) => console.log(jsonMovies)); */
+// adding cards to frontpage
 let movies;
-let val = document.querySelector("hello");
 
-val.innerHTML = ' <h1 class="majs"></h1>';
+let ul = document.querySelector(".class");
+let li;
 
-function showCards() {
-  async function display(movies) {
-    let res = await fetch("src/movies.json");
-    let data = await res.json();
+async function displayCards() {
+  let res = await fetch("src/movies.json");
+  let data = await res.json();
 
-    movies = data.movies[0];
-    console.log(movies);
-    return movies;
-  }
+  li = data.movies;
+
+  li.forEach(function (movieAll) {
+    let img = document.createElement("img");
+    img.setAttribute("src", movieAll.posterImageUrl);
+    img.setAttribute("alt", "Hej");
+    img.classList.add("cards");
+    ul.append(img);
+    console.log(img);
+  });
 }
-
-showCards(title);
+displayCards();
