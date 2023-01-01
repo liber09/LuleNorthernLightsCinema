dd2907402cc59853c7fad9c41999b2ed142e150e
Dan Saa <dansaa8@protonmail.com>
2023-01-01 10:58:32 +0100

    Add function that appends <a> to secNav from arr

--- js/secNav/secNav.js
+++ js/secNav/secNav.js
@@ -1,13 +1,41 @@
-import anchors from "./anchors.js";
-console.log(anchors);
+import { repAncs, salAncs, specAncs, liveAncs, barBistAncs } from "./anchors.js"
 
-const secNav = document.querySelector('.secNav-menu');
+const salons = document.querySelector('#salons');
+const specials = document.querySelector('#specials');
+const liveOnCinema = document.querySelector('#liveOnCinema');
+const barAndBistro = document.querySelector('#barAndBistro');
 
-const children = secNav.childNodes;
+salons.appendChild(appendDropdown(salAncs));
 
-for(var child in children){
-    if (children[child].firstChild != null) {
+function appendDropdown(arr) {
+    const dropDown = document.createElement('div');
+    dropDown.className = "secNav-dropdown";
 
-        console.log(children[child].firstChild);
+    for (let i = 0; i < arr.length; i++) {
+        const a = document.createElement('a');
+        a.innerHTML = arr[i][0];
+        a.setAttribute('href', arr[i][1]);
+        dropDown.append(a);
     }
+    return dropDown;
 }
+
+
+
+
+// <nav class="secNav">
+// <menu class="secNav-menu">
+// <li class="secNav-menu-item">
+//   <button onclick="myFunction()" class="dropBtn" id="myDropdown">Repertoar</button>
+//   <div class="secNav-dropdown">
+//     <a href="#">Link 1</a>
+//     <a href="#">Link 2</a>
+//     <a href="#">Link 3</a>
+//   </div>
+// </li>
+// <li class="secNav-menu-item"><button class="dropBtn" id="salons">Salonger</button></li>
+// <li class="secNav-menu-item"><button class="dropBtn" id="specials">Specialvisningar</button></li>
+// <li class="secNav-menu-item"><button class="dropBtn" id="liveOnCinema">Live på bio</button></li>
+// <li class="secNav-menu-item"><button class="dropBtn" id="barAndBistro">Bar & Bistro</button></li>
+// </menu>
+// </nav>
