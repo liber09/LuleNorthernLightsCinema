bb034b5f8cfb90710dfb011560c20a7324ae8334
liber09 <linda.bergsangel@hotmail.com>
2023-01-02 05:11:30 +0100

    Saloon with 12 seats in 6 rows drawn on canvas

--- js/booking/booking.js
+++ js/booking/booking.js
@@ -2,8 +2,14 @@ window.onload = function(){
     const canvas = document.querySelector('#seatPlanCanvas');
     if (canvas.getContext) {
     var context = canvas.getContext('2d');
+    //Seat columns
     for (let i = 0; i < 12; i++) {
-      context.fillRect(25+(i*60),100,20,20)
+      //Seat rows
+      for (let j = 0; j < 6; j++) {
+        context.fillRect(25+(i*60),100+(j*90),20,20) //Each seat
+      }
+
+      
     }
     
   }
