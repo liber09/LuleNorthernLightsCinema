2580035ad71083212b0ee27fb637bd77e79dadcb
liber09 <linda.bergsangel@hotmail.com>
2023-01-02 04:53:34 +0100

    Added js file and screen

---
+++ js/booking/booking.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    const canvas = document.querySelector('#seatPlanCanvas');
+    if (canvas.getContext) {
+    var context = canvas.getContext('2d');
+    
+    context.fillRect(20,20,100,100);
+    context.clearRect(40,40,60,60);
+    context.strokeRect(45,45,50,50);
+  }
+}
