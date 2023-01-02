82742e8a769db2a9f595a7fe31e011361909c2bd
Rasmus Eliasson <ras.muse@hotmail.com>
2023-01-02 08:11:13 +0100

    confict resolved fix

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ function hamburger() {
     mainNavShow.classList.remove("show");
   }
 }
+// adding cards to frontpage
+let movies;
+
+let div = document.querySelector(".picture-container");
+let li;
+let hej = document.querySelector(".title");
+
+async function displayCards() {
+  let res = await fetch("src/movies.json");
+  let data = await res.json();
+
+  li = data.movies;
+
+  li.forEach(function (movieAll) {
+    let img = document.createElement("img");
+    img.setAttribute("src", movieAll.posterImageUrl);
+    img.setAttribute("alt", "Hej");
+    img.classList.add("cards");
+    div.append(img);
+    console.log(img);
+  });
+}
+displayCards();
