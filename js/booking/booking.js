fd1df3a534ff43dae46a7d1546181ed0ab9b9fcf
liber09 <linda.bergsangel@hotmail.com>
2023-01-02 05:41:53 +0100

    Much better solution than draw on canvas

--- js/booking/booking.js
+++ js/booking/booking.js
@@ -1,16 +1,17 @@
-window.onload = function(){
-    const canvas = document.querySelector('#seatPlanCanvas');
-    if (canvas.getContext) {
-    var context = canvas.getContext('2d');
-    //Seat columns
-    for (let i = 0; i < 12; i++) {
-      //Seat rows
-      for (let j = 0; j < 6; j++) {
-        context.fillRect(25+(i*60),100+(j*90),20,20) //Each seat
-      }
+let seatPlan = document.querySelector("#seatPlanCanvas");
 
-      
+async function showSeats() {
+
+  for (let i = 0; i < 12; i++) {
+    for (let j = 0; j < 6; j++) {
+      let seat = document.createElement("div");
+      seat.classList.add("seat");
+      seatPlan.appendChild(seat);
+      console.log(seat.top);
+      console.log(seat.right);
+      console.log(seat.bottom);
+      console.log(seat.right);
     }
-    
   }
 }
+  showSeats();
