2842c804f210f033c2fdf1a17186ff18c9b435da
Rasmus Eliasson <ras.muse@hotmail.com>
2023-01-06 16:18:15 +0100

    removed dot

--- js/booking/booking.js
+++ js/booking/booking.js
@@ -5,41 +5,42 @@ let li;
 let numberOfSelected = 0;
 
 async function showSeats() {
-
-  let res = await fetch("../src/seats.json");
+  let res = await fetch("src/seats.json");
   let data = await res.json();
 
   li = data.seats;
   let count = 0;
   li.forEach(function () {
-  let seat = document.createElement("div");
+    let seat = document.createElement("div");
     seat.classList.add("seat");
-    seat.addEventListener("click", function (e){
-      if(e.target.classList.contains("seat")){
+    seat.addEventListener("click", function (e) {
+      if (e.target.classList.contains("seat")) {
         e.target.classList.add("selected");
         numberOfSelected++;
         e.target.classList.remove("seat");
-      }else if(e.target.classList.contains("selected")){
+      } else if (e.target.classList.contains("selected")) {
         e.target.classList.remove("selected");
         numberOfSelected--;
         e.target.classList.add("seat");
       }
-      if(numberOfSelected > 0 && ticketSelector.classList.contains("active") == false){
+      if (
+        numberOfSelected > 0 &&
+        ticketSelector.classList.contains("active") == false
+      ) {
         ticketSelector.classList.add("active");
       }
-      
     });
-    seat.appendChild(document.createTextNode(count+1));
-    if (li[count].wheelchair == true){
-      const image = document.createElement("img")
+    seat.appendChild(document.createTextNode(count + 1));
+    if (li[count].wheelchair == true) {
+      const image = document.createElement("img");
       image.src = "../../static/wheelchair.png";
       seat.appendChild(image);
     }
-    if (li[count].available == false){
+    if (li[count].available == false) {
       seat.classList.add("unavailable");
     }
     seatPlan.appendChild(seat);
     count++;
-  })
+  });
 }
 showSeats();
