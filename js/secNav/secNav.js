d5eb6096e191d66ac807fe1346529c54e4af743b
Dan Saa <dansaa8@protonmail.com>
2023-01-06 18:10:08 +0100

    Fix secNav

--- js/secNav/secNav.js
+++ js/secNav/secNav.js
@@ -6,59 +6,58 @@ import {
   barBistAncs,
 } from "./anchors.js";
 
+const folderDir = "/LuleNorthenLightsCinema/pages/";
 // Only pass along folderDir if the html-files are in another directory than the calling document.
-  const rep = document.querySelector("#rep");
-  const repDrop = document.querySelector("#rep-dropdown");
-  appendAnchors(rep, repDrop, repAncs);
+const rep = document.querySelector("#rep");
+const repDrop = document.querySelector("#rep-dropdown");
+appendAnchors(rep, repDrop, repAncs);
 
-  const sal = document.querySelector("#sal");
-  const salDrop = document.querySelector("#sal-dropdown");
-  appendAnchors(sal, salDrop, salAncs);
+const sal = document.querySelector("#sal");
+const salDrop = document.querySelector("#sal-dropdown");
+appendAnchors(sal, salDrop, salAncs);
 
-  const spec = document.querySelector("#spec");
-  const specDrop = document.querySelector("#spec-dropdown");
-  appendAnchors(spec, specDrop, specAncs);
+const spec = document.querySelector("#spec");
+const specDrop = document.querySelector("#spec-dropdown");
+appendAnchors(spec, specDrop, specAncs);
 
-  const live = document.querySelector("#live");
-  const liveDrop = document.querySelector("#live-dropdown");
-  appendAnchors(live, liveDrop, liveAncs);
+const live = document.querySelector("#live");
+const liveDrop = document.querySelector("#live-dropdown");
+appendAnchors(live, liveDrop, liveAncs);
 
-  const barBist = document.querySelector("#barBist");
-  const barBistDrop = document.querySelector("#barBist-dropdown");
-  appendAnchors(barBist, barBistDrop, barBistAncs);
+const barBist = document.querySelector("#barBist");
+const barBistDrop = document.querySelector("#barBist-dropdown");
+appendAnchors(barBist, barBistDrop, barBistAncs);
 
-  const folderDir = "/LuleNorthenLightsCinema/pages/";
+function appendAnchors(btn, dropDown, anchors) {
+  for (let i = 0; i < anchors.length; i++) {
+    const a = document.createElement("a");
+    a.innerHTML = anchors[i][0];
 
-  function appendAnchors(btn, dropDown, anchors) {
-    for (let i = 0; i < anchors.length; i++) {
-      const a = document.createElement("a");
-      a.innerHTML = anchors[i][0];
-
-      if (anchors[i][1] != "hrefLink" && anchors[i][1] != "") {
-          a.setAttribute("href", folderDir + anchors[i][1]);
-      }
-      dropDown.append(a);
+    if (anchors[i][1] != "hrefLink" && anchors[i][1] != "") {
+      a.setAttribute("href", folderDir + anchors[i][1]);
     }
-
-    btn.addEventListener("click", () => {
-      dropDown.classList.toggle("show");
-    });
+    dropDown.append(a);
   }
 
-  window.onclick = function (event) {
-    if (!event.target.matches("#rep")) {
-      document.querySelector("#rep-dropdown").classList.remove("show");
-    }
-    if (!event.target.matches("#sal")) {
-      document.querySelector("#sal-dropdown").classList.remove("show");
-    }
-    if (!event.target.matches("#spec")) {
-      document.querySelector("#spec-dropdown").classList.remove("show");
-    }
-    if (!event.target.matches("#live")) {
-      document.querySelector("#live-dropdown").classList.remove("show");
-    }
-    if (!event.target.matches("#barBist")) {
-      document.querySelector("#barBist-dropdown").classList.remove("show");
-    }
-  };
+  btn.addEventListener("click", () => {
+    dropDown.classList.toggle("show");
+  });
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches("#rep")) {
+    document.querySelector("#rep-dropdown").classList.remove("show");
+  }
+  if (!event.target.matches("#sal")) {
+    document.querySelector("#sal-dropdown").classList.remove("show");
+  }
+  if (!event.target.matches("#spec")) {
+    document.querySelector("#spec-dropdown").classList.remove("show");
+  }
+  if (!event.target.matches("#live")) {
+    document.querySelector("#live-dropdown").classList.remove("show");
+  }
+  if (!event.target.matches("#barBist")) {
+    document.querySelector("#barBist-dropdown").classList.remove("show");
+  }
+};
